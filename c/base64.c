8a91878d813552cea1e4c89ae305b38bd8abac07
alexsandrovp <alexsandrovp@googlemail.com>
2016-02-10 16:53:10 -0200

    Create base64.c

---
+++ c/base64.c
@@ -0,0 +1,129 @@
+#include "base64.h"
+
+const unsigned char encodelookup[] = {
+	0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48, 0x49, 0x4A, 0x4B, 0x4C, 0x4D, 0x4E, 0x4F, 0x50, 0x51, 0x52, 0x53, 0x54, 0x55, 0x56, 0x57, 0x58, 0x59, 0x5A,
+	0x61, 0x62, 0x63, 0x64, 0x65, 0x66, 0x67, 0x68, 0x69, 0x6A, 0x6B, 0x6C, 0x6D, 0x6E, 0x6F, 0x70, 0x71, 0x72, 0x73, 0x74, 0x75, 0x76, 0x77, 0x78, 0x79, 0x7A,
+	0x30, 0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x2B, 0x2F
+};
+
+const unsigned char decodelookup[] = {
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0x3E, 0xFF, 0xFF, 0xFF, 0x3F,
+	0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x3A, 0x3B, 0x3C, 0x3D, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E,
+	0x0F, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF,
+	0xFF, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F, 0x20, 0x21, 0x22, 0x23, 0x24, 0x25, 0x26, 0x27, 0x28,
+	0x29, 0x2A, 0x2B, 0x2C, 0x2D, 0x2E, 0x2F, 0x30, 0x31, 0x32, 0x33
+};
+
+size_t base64_encode(const char* plain, size_t plain_length, char** encoded)
+{
+	//3 octets (bytes) match 4 sextets (bitcount -> 3*8 == 4*6)
+	size_t bitcount = plain_length * 4;
+	size_t remainder = plain_length % 3;
+	size_t limit = plain_length - remainder - 1;
+	const size_t encoded_length = (bitcount % 3) > 0 ? 1 + bitcount / 3 : bitcount / 3;
+	(*encoded) = new char[encoded_length + 1];
+	(*encoded)[encoded_length] = 0;
+
+	size_t i = 0, j = 0;
+	for (; i < limit; i += 3, j += 4)
+	{
+		(*encoded)[j] = encodelookup[(plain[i] & 0xFC) >> 2];
+		(*encoded)[j + 1] = encodelookup[((plain[i] & 0x03) << 4) | ((plain[i + 1] & 0xF0) >> 4)];
+		(*encoded)[j + 2] = encodelookup[((plain[i + 1] & 0x0F) << 2) | ((plain[i + 2] & 0xC0) >> 6)];
+		(*encoded)[j + 3] = encodelookup[plain[i + 2] & 0x3F];
+	}
+
+	if (remainder-- > 0)
+	{
+		(*encoded)[j] = encodelookup[(plain[i] & 0xFC) >> 2];
+		if (remainder > 0)
+		{
+			(*encoded)[j + 1] = encodelookup[((plain[i] & 0x03) << 4) | ((plain[i + 1] & 0xF0) >> 4)];
+			(*encoded)[j + 2] = encodelookup[((plain[i + 1] & 0x0F) << 2)];
+		}
+		else
+		{
+			(*encoded)[j + 1] = encodelookup[((plain[i] & 0x03) << 4)];
+		}
+	}
+
+	return encoded_length;
+}
+
+size_t base64_decode(const char* encoded, size_t encoded_length, char** plain)
+{
+	size_t error_index = -1;
+	const size_t bitcount = encoded_length * 3;
+	const size_t plain_length = bitcount / 4;
+	(*plain) = new char[plain_length + 1];
+	(*plain)[plain_length] = 0;
+
+	size_t remainder = plain_length % 3;
+	size_t limit = plain_length - remainder - 1;
+
+	size_t i = 0, j = 0;
+	for (; i < limit; i += 3, j += 4)
+	{
+		if (decodelookup[encoded[j]] == 0xFF)
+		{
+			error_index = j;
+			goto error;
+		}
+		if (decodelookup[encoded[j + 1]] == 0xFF)
+		{
+			error_index = j + 1;
+			goto error;
+		}
+		if (decodelookup[encoded[j + 2]] == 0xFF)
+		{
+			error_index = j + 2;
+			goto error;
+		}
+		if (decodelookup[encoded[j + 3]] == 0xFF)
+		{
+			error_index = j + 3;
+			goto error;
+		}
+
+		(*plain)[i] = (char)(((decodelookup[encoded[j]] & 0x3F) << 2) | ((decodelookup[encoded[j + 1]] & 0x30) >> 4));
+		(*plain)[i + 1] = (char)(((decodelookup[encoded[j + 1]] & 0x0F) << 4) | ((decodelookup[encoded[j + 2]] & 0x3C) >> 2));
+		(*plain)[i + 2] = (char)(((decodelookup[encoded[j + 2]] & 0x03) << 6) | (decodelookup[encoded[j + 3]] & 0x3F));
+	}
+
+	if (remainder-- > 0)
+	{
+		if (decodelookup[encoded[j]] == 0xFF)
+		{
+			error_index = j;
+			goto error;
+		}
+		if (decodelookup[encoded[j + 1]] == 0xFF)
+		{
+			error_index = j + 1;
+			goto error;
+		}
+
+		(*plain)[i] = (char)(((decodelookup[encoded[j]] & 0x3F) << 2) | ((decodelookup[encoded[j + 1]] & 0x30) >> 4));
+
+		if (remainder > 0)
+		{
+			if (decodelookup[encoded[j + 2]] == 0xFF)
+			{
+				error_index = j + 2;
+				goto error;
+			}
+
+			(*plain)[i + 1] = (char)(((decodelookup[encoded[j + 1]] & 0x0F) << 4) | ((decodelookup[encoded[j + 2]] & 0x3C) >> 2));
+		}
+	}
+
+	return plain_length;
+
+error:
+	delete[] (*plain);
+	*plain = 0;
+	return error_index;
+}
